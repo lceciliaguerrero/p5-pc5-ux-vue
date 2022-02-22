76cc8aa5652e2ac773e7990785b6ba8c64123e36
Laura Cecilia Guerrero <lcecilia@cifpfbmoll.eu>
2022-02-22 18:27:42 +0100

    feat and refactor:
    Modifico form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,12 +7,12 @@
           <div class="col-md-6 mb-3">
             <label for="firstName">Nombre</label>
             <input id="firstName" class="form-control" type="text" name="firstName"
-                   placeholder="Escriba su nombre de pila" v-model="nombrePila" aria-required="true">
+                   placeholder="Escriba su nombre de pila" v-model="nombrePila" aria-required="true" required>
           </div>
           <div class="col-md-6 mb-3">
             <label for="lastName">Apellidos</label>
             <input id="lastName" class="form-control" type="text" name="lastName"
-                   placeholder="Escriba sus apellidos de pila" v-model="apellidoPila" aria-required="true">
+                   placeholder="Escriba sus apellidos de pila" v-model="apellidoPila" aria-required="true" required>
           </div>
         </div>
         <div class="mb-3">
@@ -22,7 +22,7 @@
               <span class="input-group-text">@</span>
             </div>
             <input id="username" class="form-control" type="text" name="username"
-                   placeholder="Escriba su nombre de usuario" v-model="nombreUsuario" aria-required="true">
+                   placeholder="Escriba su nombre de usuario" v-model="nombreUsuario" aria-required="true" required>
           </div>
         </div>
         <div class="mb-3">
@@ -37,19 +37,19 @@
           <div class="custom-control custom-radio">
             <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" value="credito"
                    v-model="metodoPago"
-                   aria-required="true">
+                   aria-required="true" required>
             <label class="custom-control-label" for="credit">Tarjeta de Crédito</label>
           </div>
           <div class="custom-control custom-radio">
             <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" value="debito"
                    v-model="metodoPago"
-                   aria-required="true">
+                   aria-required="true" required>
             <label class="custom-control-label" for="debit">Tarjeta de Debito</label>
           </div>
           <div class="custom-control custom-radio">
             <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" value="paypal"
                    v-model="metodoPago"
-                   aria-required="true">
+                   aria-required="true" required>
             <label class="custom-control-label" for="paypal">PayPal</label>
           </div>
         </div>
@@ -60,24 +60,24 @@
           <div class="col-md-6 mb-3">
             <label for="cc-text">Nombre de la Tarjeta</label>
             <input type="text" class="form-control" id="cc-text" v-model="nombreTarjeta"
-                   placeholder="Escribe el nombre de la tarjeta" aria-required="true">
+                   placeholder="Escribe el nombre de la tarjeta" aria-required="true" required>
           </div>
           <div class="col-md-6 mb-3">
             <label for="cc-number">Numero de la Tarjeta</label>
             <input type="text" class="form-control" id="cc-number" v-model="numeroTarjeta"
-                   placeholder="Escribe el numero de la tarjeta" aria-required="true">
+                   placeholder="Escribe el numero de la tarjeta" aria-required="true" required>
           </div>
         </div>
         <div class="mb-3">
           <label for="cc-expiration">Fecha de Caducidad de la Tarjeta</label>
           <input type="date" class="form-control" id="cc-expiration" v-model="numeroExpiracionTarjeta"
-                 aria-required="true">
+                 aria-required="true" required>
         </div>
         <div class="mb-3">
           <label for="cc-cvv">Valor de Verificación de la Tarjeta</label>
           <input type="text" class="form-control" id="cc-cvv" v-model="valorVerificacionTarjeta"
                  placeholder="Escribe el numero de verificación de la tarjeta"
-                 aria-required="true">
+                 aria-required="true" required>
         </div>
       </div>
       <input
